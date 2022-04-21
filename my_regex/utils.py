80b5c3a9e29de33910ceefba7e6baf627e27f524
Abdelrhman-Hosny <ahosny2792@gmail.com>
2022-04-21 16:03:47 +0200

    match parenthesis and brackets

--- my_regex/utils.py
+++ my_regex/utils.py
@@ -1,6 +1,37 @@
 import json
 
+
 def write_dict_to_json(d, filename):
-    with open(filename, 'w') as f:
+    with open(filename, "w") as f:
         json.dump(d, f)
 
+
+def match_2_chars(regex, char1, char2):
+
+    matches = []
+    my_stack = []
+
+    for i, c in enumerate(regex):
+        if (c == char1 and (i == 0 or regex[i-1] != "\\")):
+            my_stack.append(i)
+        elif (c == char2 and (i == 0 or regex[i-1] != "\\")):
+            if len(my_stack) == 0:
+                raise Exception(f"Unmatched {char2}")
+            matches.append((my_stack.pop(), i))
+    if len(my_stack) > 0:
+        raise Exception(f"Unmatched {char1}")
+    return matches
+
+def get_matched_parenthesis(regex):
+    """
+    Returns a list of tuples, each tuple has the start and end for each index
+    """
+
+    return match_2_chars(regex, "(", ")")
+
+
+def get_matched_square_brackets(regex):
+    """
+    Returns a list of tuples, each tuple has the start and end for each index
+    """
+    return match_2_chars(regex, "[", "]")
