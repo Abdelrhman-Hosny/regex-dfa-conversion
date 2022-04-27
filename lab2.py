a6b131ee93630253bcef689382114337ff1dad80
Abdelrhman-Hosny <ahosny2792@gmail.com>
2022-04-27 16:43:05 +0200

    added or but doesn't work with other operators

--- lab2.py
+++ lab2.py
@@ -1,6 +1,8 @@
 from my_regex.utils import (
     write_dict_to_json,
-    get_num_of_last_state
+    get_num_of_last_state,
+    add_key_val,
+    detect_or_positions
 )
 
 from my_regex.draw import draw_states
@@ -16,15 +18,12 @@ def construct_nfa(regex):
 
     current_state, NFA = get_regex(regex, NFA, 0)
 
-    NFA[f"S{get_num_of_last_state(NFA)}"] = {"acceptingState": True}
+    add_key_val(NFA, f'S{get_num_of_last_state(NFA)}', 'acceptingState', True)
     NFA["startingState"] = "S0"
     return NFA
 
 
-NFA = construct_nfa("[ab]*")
+NFA = construct_nfa("ab+|cd|ef")
+write_dict_to_json(NFA, "nfa.json")
 print(NFA)
 draw_states(NFA)
-# print(construct_nfa("([qs])([wqeqwdwq])"))
-# print(construct_nfa("a(((ys)(we))(xp))c"))
-# print(zero_or_more("ab", {"S0": {}}, 0))
-# print(optional("(ab)*", {"S0": {}}, 0))
--- my_regex/operators.py
+++ my_regex/operators.py
@@ -5,6 +5,7 @@ from my_regex.utils import (
     get_next_brackets,
     detect_empty_nfa,
     get_num_of_last_state,
+    get_split_indices_for_or_operation,
 )
 
 
@@ -12,53 +13,85 @@ def get_regex(regex, NFA, current_state, in_brackets=False, bracket_type=None):
 
     i = 0
 
-    brackets = get_brackets(regex)
-    (current_outer_bracket, current_inner_brackets) = get_next_brackets(regex, brackets)
-    while i < len(regex):
-        # checking for alnum
-        for char in regex[i:]:
-            # if char is alphanumeric, we add the transition else break
-            if not char.isalnum():
-                break
+    start_state = current_state
 
-            if (i+1) < len(regex) and regex[i+1] in regex_operators.keys():
-                current_state, _ = regex_operators[regex[i+1]](char, NFA, current_state, regex_fn=get_regex)
+    split_indices = get_split_indices_for_or_operation(regex)
+
+    if split_indices:
+        split_indices = [-1] + split_indices
+        split_indices.append(len(regex) + 1)
+
+        new_split_indices = [
+            (split_indices[i], split_indices[i + 1])
+            for i in range(len(split_indices) - 1)
+        ]
+
+        regex_arr = [regex[x[0] + 1: x[1]] for x in new_split_indices]
+
+        current_state, NFA = or_operator(regex_arr, NFA, current_state, start_state)
+    else:
+        brackets = get_brackets(regex)
+        (current_outer_bracket, current_inner_brackets) = get_next_brackets(
+            regex, brackets
+        )
+        while i < len(regex):
+            # checking for alnum
+            for char in regex[i:]:
+                # if char is alphanumeric, we add the transition else break
+                if not char.isalnum():
+                    break
+
+                if (i + 1) < len(regex) and regex[i + 1] in regex_operators.keys():
+                    current_state, _ = regex_operators[regex[i + 1]](
+                        char, NFA, current_state, regex_fn=get_regex
+                    )
+                    i += 1
+                else:
+                    set_alnum_state(
+                        char, NFA, f"S{current_state}", f"S{current_state + 1}"
+                    )
+
+                if bracket_type == "(" or bracket_type is None:
+                    current_state += 1
                 i += 1
-            else:
-                set_alnum_state(char, NFA, f"S{current_state}", f"S{current_state + 1}")
-
-            if bracket_type == '(' or bracket_type is None:
+            if bracket_type == "[":
                 current_state += 1
-            i += 1
-        if bracket_type == '[':
-            current_state += 1
-        # checking for brackets
-        if current_outer_bracket and i == current_outer_bracket[0]:
+            # checking for brackets
+            if current_outer_bracket and i == current_outer_bracket[0]:
 
-            if (i+1) < len(regex) and regex[current_outer_bracket[1] + 1] in regex_operators.keys():
-                current_state, new_NFA = regex_operators[regex[current_outer_bracket[1] + 1]](regex[i+1: current_outer_bracket[1]], NFA, current_state, bracket_type=regex[i], regex_fn=get_regex)
-                i = current_outer_bracket[1] + 2
-            else:
-                # we are at the start of the outer bracket
-                current_state, new_NFA = get_regex(
-                    regex[i + 1: current_outer_bracket[1]],
-                    {f"S{current_state}": {}},
-                    current_state,
-                    True,
+                if (current_outer_bracket[1] + 1) < len(regex) and regex[
+                    current_outer_bracket[1] + 1
+                ] in regex_operators.keys():
+                    current_state, new_NFA = regex_operators[
+                        regex[current_outer_bracket[1] + 1]
+                    ](
+                        regex[i + 1 : current_outer_bracket[1]],
+                        NFA,
+                        current_state,
+                        bracket_type=regex[i],
+                        regex_fn=get_regex,
+                    )
+                    i = current_outer_bracket[1] + 2
+                else:
+                    # we are at the start of the outer bracket
+                    current_state, new_NFA = get_regex(
+                        regex[i + 1 : current_outer_bracket[1]],
+                        {f"S{current_state}": {}},
+                        current_state,
+                        True,
+                    )
+                    i = current_outer_bracket[1] + 1
+                if detect_empty_nfa(NFA):
+                    NFA = new_NFA
+                    current_state = get_num_of_last_state(NFA) + 1
+
+                elif not detect_empty_nfa(new_NFA):
+                    NFA = {**NFA, **new_NFA}
+                    current_state = get_num_of_last_state(NFA) + 1
+
+                (current_outer_bracket, current_inner_brackets) = get_next_brackets(
+                    regex, brackets
                 )
-                i = current_outer_bracket[1] + 1
-            if detect_empty_nfa(NFA):
-                NFA = new_NFA
-                current_state = get_num_of_last_state(NFA) + 1
-
-            elif not detect_empty_nfa(new_NFA):
-                NFA = {**NFA, **new_NFA}
-                current_state = get_num_of_last_state(NFA) + 1
-
-            (current_outer_bracket, current_inner_brackets) = get_next_brackets(
-                regex, brackets
-            )
-
 
     return current_state, NFA
 
@@ -69,12 +102,20 @@ def zero_or_more(regex, NFA, current_state, bracket_type=None, regex_fn=get_rege
     current_state += 1
     start_state = current_state
     new_state, new_NFA = regex_fn(regex, NFA, start_state, bracket_type=bracket_type)
-    
+
     add_key_val(NFA, f"S{start_state}", EPSILON, f"S{new_state + 1}")
     add_key_val(NFA, f"S{new_state}", EPSILON, f"S{start_state}")
     add_key_val(NFA, f"S{new_state}", EPSILON, f"S{new_state + 1}")
     NFA[f"S{new_state + 1}"] = dict()
     NFA = {**NFA, **new_NFA}
+    return new_state, NFA
+
+
+def one_or_more(regex, NFA, current_state, bracket_type=None, regex_fn=get_regex):
+    start_state = current_state
+    new_state, new_NFA = regex_fn(regex, NFA, current_state, bracket_type=bracket_type)
+
+    add_key_val(NFA, f"S{new_state}", EPSILON, f"S{start_state}")
     return new_state, NFA
 
 
@@ -88,12 +129,24 @@ def optional(regex, NFA, current_state, bracket_type=None, regex_fn=get_regex):
     add_key_val(NFA, f"S{start_state}", EPSILON, f"S{current_state}")
     return current_state, NFA
 
-def one_or_more(regex, NFA, current_state, regex_fn=get_regex):
-    pass
+
+def or_operator(
+    regex_arr, NFA, current_state, start_state, bracket_type=None, regex_fn=get_regex
+):
+
+    end_states = []
+    for regex in regex_arr:
+        current_state += 1
+        add_key_val(NFA, f"S{start_state}", EPSILON, f"S{current_state}")
+        current_state, NFA = regex_fn(
+            regex, NFA, current_state, bracket_type=bracket_type
+        )
+        end_states.append(current_state)
+    current_state += 1
+    for end_state in end_states:
+        add_key_val(NFA, f"S{end_state}", EPSILON, f"S{current_state}")
+
+    return current_state, NFA
 
 
-regex_operators = {
-    "*": zero_or_more,
-    "?": optional,
-    "+": one_or_more
-}
+regex_operators = {"*": zero_or_more, "?": optional, "+": one_or_more}
