29833f941534bde9db227adbfdfc194e6ce3aa16
Abdelrhman-Hosny <ahosny2792@gmail.com>
2022-05-01 12:32:39 +0200

    add validation for repeat character

--- my_regex/validation.py
+++ my_regex/validation.py
@@ -7,13 +7,37 @@ class RegexValidator:
 
         self.square_brackets = get_matched_square_brackets(regex)
         self.parenthesis = get_matched_parenthesis(regex)
-
         self.sorted_brackets = sorted(self.square_brackets + self.parenthesis, key=lambda x: x[0])
+        self.asterisk_indices = [i for i, char in enumerate(regex) if char == '*']
+        self.plus_indices = [i for i, char in enumerate(regex) if char == '+']
+        self.question_indices = [i for i, char in enumerate(regex) if char == '?']
 
     def validate(self):
         validate_square_brackets(self.square_brackets)
         validate_sorted_brackets(self.sorted_brackets)
+        self.check_repeat_chars()
 
+    def get_repeat_char_indices(self):
+        return sorted(self.asterisk_indices + self.plus_indices + self.question_indices)
+
+    def check_repeat_chars(self):
+        repeat_char_indices = self.get_repeat_char_indices()
+        if len(repeat_char_indices) < 2:
+            return
+
+        repeated_chars = [ repeat_char_indices[i] for i in range(len(repeat_char_indices) - 1)
+                            if repeat_char_indices[i] + 1 == repeat_char_indices[i + 1]]
+        
+        if repeated_chars:
+            # check if between square brackets
+            for i in repeated_chars:
+                in_brackets = False
+                for bracket in self.square_brackets:
+                    if bracket[0] < i < bracket[1]:
+                        in_brackets = True
+                        break
+                if not in_brackets:
+                    raise Exception("Invalid regex, repeated chars (+*?) cannot follow each other outside of square brackets")
 
 def validate_square_brackets(square_brackets):
     """
@@ -49,3 +73,4 @@ def validate_sorted_brackets(brackets):
 
     if intersecting_brackets != 0:
         raise Exception("Brackets intersect")
+
